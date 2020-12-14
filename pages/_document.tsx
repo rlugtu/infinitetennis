d6df881d4848d2990fb9fc5e6b5279cc535309d9
Ryan Lugtu <ryanlugtu@Ryans-MBP.fios-router.home>
2020-12-14 02:18:38 -0500

    installed typescript and fixed documenttsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class CustomDocument extends Document { 
     render() {
+        return(
         <Html>
             <Head>
                 <meta property="custom" content ="test">
                 </meta>
             </Head>
                 <body>
-                    <Main>
-                    </Main>
+                    <Main></Main>
                 </body>
         </Html>
+        )
     }
 }
