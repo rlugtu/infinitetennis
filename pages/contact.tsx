f6ee487d9981463d969e9ef4e64aa7f881841ec8
Ryan Lugtu <ryanlugtu@Ryans-MBP.fios-router.home>
2021-01-12 15:04:29 -0500

    cleaned up some code

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,7 +3,7 @@ import Footer from '../components/Footer'
 import Banner from '../components/Banner'
 import styles from '../components/modulecss/Contact.module.css'
 import emailjs from 'emailjs-com'
-
+import ContactForm from '../components/Contact'
 import Image from 'next/image'
 const Contact = () => {
     const sendEmail = (e) => {
@@ -33,21 +33,7 @@ const Contact = () => {
             {/* <Banner /> */}
             <div className={styles.contactContainer}>
                 <div className={styles.contactContent}>
-                    <div className={styles.contactFormContainer}>
-                        <h1>Get In Touch</h1>
-                        <p>Please fill out this form and we will be in touch</p>
-                        <form className={styles.contactForm} onSubmit={sendEmail}>
-                            <label>Name</label>
-                            <input className={styles.input} type="text" name="name" />
-                            <label>Email</label>
-                            <input className={styles.input}type="email" name="email" />
-                            <label>Phone Number</label>
-                            <input className={styles.input}type="number" name="phone" />
-                            <label>Message</label>
-                            <textarea className={styles.textArea} name="message" rows={5} cols={5}/>
-                            <input className={styles.submitButton}type="submit" value="Send" />
-                        </form>
-                    </div>
+                    <ContactForm />
                     <div className={styles.connectContainer}>
                         <h1>Connect with us</h1>
                         <p>For questions and inquiries</p>
