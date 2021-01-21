a04e91473233c9bc12134e2a138f65a3dda9ae87
Ryan Lugtu <ryanlugtu@Ryans-MBP.fios-router.home>
2021-01-20 22:33:41 -0500

    checked alumni for import error

--- pages/alumni.tsx
+++ pages/alumni.tsx
@@ -1,10 +1,8 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import styles from '../components/modulecss/Alumni.module.css'
-import Image from "next/image"
 import Prospects from '../components/Prospects'
 import Bios from '../components/assets/prospects.json'
-import Prospects2 from '../components/ProspectsNoPic'
 const Alumni = () => {
     return (
         <div>
